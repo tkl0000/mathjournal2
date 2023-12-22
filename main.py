c7574d5248a9068e2fff247472a7768d48ab79ed
tkl0000 <thomasli2025@gmail.com>
2023-12-21 22:58:22 -0500

    sdfouHSOUHFOh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import math
+import numpy as np
 
 N = 5000
 
@@ -12,24 +14,31 @@ def sieve():
                 prime[x] = False
     return prime
 
-def main():
-
+def static():
     global N
     
-    x_values = []
-    y_values = []
-    prime = sieve()
-    for i in range(1, N):
-        x_values.append(i * math.cos(i))
-        y_values.append(i * math.sin(i))
-        # x_values.append(i * math.cos(i))
-        # y_values.append(i * mathcode.sin(i))
+    x_values = [i * math.cos(i) for i in range(1, N)]
+    y_values = [i * math.sin(i) for i in range(1, N)]
 
     plt.figure(1)
     plt.grid(True)
     plt.axhline(0, color='black',linewidth=0.5)  # X-axis line
     plt.axvline(0, color='black',linewidth=0.5)  # Y-axis line
     plt.scatter(x_values, y_values, color='red', marker='.', label='Dots')
+    
+    prime = sieve()
+    x_primes = [i * math.cos(i) for i in range(1, N) if prime[i]]
+    y_primes = [i * math.sin(i) for i in range(1, N) if prime[i]]
+
+    fig = plt.subplots()
+    plt.grid(True)
+    plt.axhline(0, color='black',linewidth=0.5)  # X-axis line
+    plt.axvline(0, color='black',linewidth=0.5)  # Y-axis line
+    plt.scatter(x_primes, y_primes, color='blue', marker='.', label='Dots')
+    plt.show()
+
+def animate():
+    global N
     
     x_primes = []
     y_primes = []
@@ -40,16 +49,50 @@ def main():
             x_primes.append(i * math.sin(i))
         # x_values.append(i * math.cos(i))
         # y_values.append(i * math.sin(i))
+            
+    i_to_plot = [i for i in range(1, N)]
 
-    plt.figure(2)
+    fig, ax = plt.subplots()
+    scat = ax.scatter(0, 0, color='blue', marker='.', label='Dots')
+    plt.xlim(-5000, 5000)
+    plt.ylim(-5000, 5000)
     plt.grid(True)
     plt.axhline(0, color='black',linewidth=0.5)  # X-axis line
     plt.axvline(0, color='black',linewidth=0.5)  # Y-axis line
-    plt.scatter(x_primes, y_primes, color='blue', marker='.', label='Dots')
+    
 
+    def update(frame, i_to_plot):
+        if (frame == 0): i_to_plot = [i for i in range(1, N)]
+        if (not prime[frame] and frame in i_to_plot):
+            i_to_plot.remove(frame)
+        x_plot = [i * math.cos(i) for i in i_to_plot]
+        y_plot = [i * math.sin(i) for i in i_to_plot]
+        scat.set_offsets(np.c_[x_plot, y_plot])
+        print(frame)
 
-
-
+    ani = animation.FuncAnimation(fig, update, frames=N, fargs=[i_to_plot], interval=0, repeat=True)
     plt.show()
+
+def ex():
+    np.random.seed(0)
+    x_data = np.random.rand(100)
+    y_data = np.random.rand(100)
+
+    fig, ax = plt.subplots()
+    scat = ax.scatter(x_data, y_data)
+
+    def update(frame):
+        # Updating the data for the scatter plot
+        scat.set_offsets(np.c_[np.random.rand(100), np.random.rand(100)])
+        return scat,
+
+    # Creating an animation
+    ani = animation.FuncAnimation(fig, update, frames=10, interval=200, blit=True)
+    plt.show()
+
+def main():
+    static()
+    animate()
+    # ex()
 
 main()
